105cfbfc89a1dd6296ade8cd12643c5330cdec50
fdyuandong <dy283090@alibaba-inc.com>
2025-04-21 18:33:18 +0800

    feat: release Modelscope and OpenAvatarChat

--- scripts/install/install_cu121.sh
+++ scripts/install/install_cu121.sh
@@ -6,4 +6,4 @@ pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https
 pip install -r requirements.txt
 
 # install H5-render
-pip install wheels/gradio_gaussian_render-0.0.2-py3-none-any.whl
+pip install wheels/gradio_gaussian_render-0.0.3-py3-none-any.whl
